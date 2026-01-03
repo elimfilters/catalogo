e2890153279ca1b5fc53d54abf3d9ceea27100d5
elimfilters <221251498+elimfilters@users.noreply.github.com>
2026-01-03 16:23:25 -0600

    Delete elimfilters-search-engine directory

--- elimfilters-search-engine/assets/script.js
+++
@@ -1,8 +0,0 @@
-﻿jQuery(document).ready(function($) {
-    const hints = { 'part': 'Search by ELIMFILTERS or other part number', 'vin': 'Enter 17-character VIN', 'equipment': 'Enter equipment model or series' };
-    $('.efs-tab').click(function() {
-        $('.efs-tab').removeClass('active');
-        $(this).addClass('active');
-        $('.efs-input').attr('placeholder', hints[$(this).data('tab')]);
-    });
-});
